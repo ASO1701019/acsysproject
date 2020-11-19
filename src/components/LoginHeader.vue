e73b6d92ecb79f230d23592ee720ec660440708f
raige <raigekka0128@yahoo.co.jp>
2020-11-20 03:34:48 +0900

    +Update LoginHeader.vueのエラーを解決

--- src/components/LoginHeader.vue
+++ src/components/LoginHeader.vue
@@ -2,13 +2,14 @@
     <div>
         <b-navbar toggleable="lg" variant="dark" type="dark">
             <a class="navbar-brand " href='/savecalorie'>
-                <!--タイトルボタン-->
+                <!--ファビコン-->
                 <img src="../../public/favicon.png" width="35" height="35" class="d-inline-block align-top" alt="penguin">
                 acsys</a>
+
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
-                <!--左側のメニュー-->
                 <b-navbar-nav>
+                    <!--左側のメニュー-->
                     <b-nav-item href="/calendar">カレンダー</b-nav-item>
                     <b-nav-item href="/statistics">統計</b-nav-item>
                     <b-nav-item href="/training">トレーニング</b-nav-item>
@@ -17,9 +18,9 @@
 
                 <b-navbar-nav class="ml-auto">
                     <b-nav-item-dropdown right>
-                        <!-- 右側のメニュー -->
+                        <!--右側のメニュー-->
                         <template v-slot:button-content>
-                            <!--名前の取得-->
+                            <!--ユーザー名-->
                             <em>{{userName}}</em>
                         </template>
                         <!--ドロップダウンメニュー-->
@@ -30,7 +31,7 @@
             </b-collapse>
         </b-navbar>
 
-        <!--ログアウト-->
+        <!--ログアウトのモーダル-->
         <div class="example-modal-window">
             <!-- コンポーネント MyModal -->
             <MyModal @close="closeModal" v-if="modal">
@@ -49,7 +50,7 @@
 </template>
 
 <script>
-
+    //モーダル
     import MyModal from './MyModal'
 
     export default {
@@ -57,14 +58,14 @@
         data() {
             return {
                 modal: false,
-                //通信に使用する関数
+                //通信に関数
                 dataGet:[],
                 userToken:this.$store.state.accountToken,
                 userInfBox:[],
             }
         },
         async created() {
-            <!--ユーザー情報をストアに保存する処理-->
+            //ユーザー情報取得
             const URL = "https://fat3lak1i2.execute-api.us-east-1.amazonaws.com/acsys/users/information"
             this.dataGet={
                 account_token:this.userToken
@@ -78,10 +79,11 @@
             })
                 .then(response => response.json())
                 .then(data => {
-                    // 通信に成功したらストアに保存
+                    // console.log(data)
                     const flg_data = data['isSuccess']
                     if(flg_data){
                         console.log('ユーザー情報取得:ok')
+                        //ストアにユーザー情報を保存する処理
                         this.userInfBox={
                             name:data['account_name'],
                             birthday:data['account_birthday'],
@@ -102,15 +104,15 @@
                 })
         },
         methods:{
-            //モーダルに関してのメソッド
+            //モーダルに関する処理
             openModal(){
                 this.modal = true
             },
             closeModal() {
                 this.modal = false
             },
+            //ログアウト
             logout(){
-                //ログアウトした時の通信
                 const URL = "https://fat3lak1i2.execute-api.us-east-1.amazonaws.com/acsys/users/logout"
                 this.dataGet={
                     account_token:this.userToken
@@ -128,7 +130,7 @@
                         const flg_data = data['isSuccess']
                         if(flg_data){
                             console.log('ログアウト:ok')
-                            //ストアの情報を削除
+                            //ログアウト時にユーザー情報を削除
                             this.$store.commit("tokenDelete")
                             this.$router.replace("/")
                         }else {
